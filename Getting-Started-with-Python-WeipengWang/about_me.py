c1507d758c582abfac911c307205bd133cb58bee
Will Wang <wewang5000forwork@gmail.com>
2022-07-19 15:32:15 -0500

    get rid werid folder

---
+++ Getting-Started-with-Python-WeipengWang/about_me.py
@@ -0,0 +1,19 @@
+full_name = 'William Weipeng Wang'
+favorite_nickname = 'k'
+age = 21
+python_experience = True
+
+favorite_hobbies = [
+  'boxing', 'day game', 'reading book', 'selfdevelopment'
+]
+
+favorite_thing = {
+  'country' : 'ecuode',
+  'food' : 'meat',
+  'hobby': favorite_hobbies[1],
+  'reading book' : 'steve job'
+}
+
+print(full_name, favorite_nickname, '\n', age, python_experience)
+
+all_vars = dict(vars())
